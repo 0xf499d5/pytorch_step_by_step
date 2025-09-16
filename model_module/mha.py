c0a558153a3627d832d9df46d1507894fe95a32a
0xf499d5 <f499d5@gmail.com>
2025-09-16 17:39:04 +0800

    浏览完第9章上：序列到序列

---
+++ model_module/mha.py
@@ -0,0 +1,30 @@
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model, input_dim=None, proj_values=True):
+        super().__init__()
+        self.linear_out = nn.Linear(n_heads * d_model, d_model)
+        self.attn_heads = nn.ModuleList([Attention(d_model, 
+                                                   input_dim=input_dim, 
+                                                   proj_values=proj_values) 
+                                         for _ in range(n_heads)])
+
+    def init_keys(self, key):
+        for attn in self.attn_heads:
+            attn.init_keys(key)
+
+    @property
+    def alphas(self):
+        # Shape: n_heads, N, 1, L (source)
+        return torch.stack([attn.alphas for attn in self.attn_heads], dim=0)
+
+    def output_function(self, contexts):
+        # N, 1, n_heads * D
+        concatenated = torch.cat(contexts, axis=-1)
+        # Linear transf. to go back to original dimension
+        out = self.linear_out(concatenated) # N, 1, D
+        return out
+
+    def forward(self, query, mask=None):
+        contexts = [attn(query, mask=mask) for attn in self.attn_heads]
+        out = self.output_function(contexts)
+        return out
